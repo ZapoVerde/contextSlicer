91d7ff7f28794ec8ea940decf5db366f8fc83bb8
strong.jeremy <strong.jeremy@gmail.com>
2025-09-30 04:42:31 +0000

    build: Align ESLint config with monorepo root

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,37 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 
-export default [
-  // This line includes all the standard recommended rules
-  ...tseslint.configs.recommended,
-  
-  // This next section adds our custom exception
+export default tseslint.config(
   {
+    ignores: ["dist/**"],
+  },
+  {
+    files: ['**/*.{ts,tsx}'],
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+    },
     rules: {
+      ...js.configs.recommended.rules,
+      ...tseslint.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': 'warn',
       '@typescript-eslint/no-unused-vars': [
         'error',
-        { 'argsIgnorePattern': '^_' } // This tells the linter to ignore unused variables starting with _
+        {
+          'varsIgnorePattern': '^_',
+          'argsIgnorePattern': '^_',
+          'caughtErrorsIgnorePattern': '^_', 
+        },
       ],
     },
-  },
-];
+  }
+);
