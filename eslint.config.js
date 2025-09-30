c17d37284889dff0eb01a02631bced6ad2eadd49
strong.jeremy <strong.jeremy@gmail.com>
2025-09-30 04:32:38 +0000

    style: Configure lint rule to allow unused vars with underscore

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,16 @@
 import tseslint from 'typescript-eslint';
 
 export default [
+  // This line includes all the standard recommended rules
   ...tseslint.configs.recommended,
+  
+  // This next section adds our custom exception
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { 'argsIgnorePattern': '^_' } // This tells the linter to ignore unused variables starting with _
+      ],
+    },
+  },
 ];
